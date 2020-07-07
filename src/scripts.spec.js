d79fdf2cc8cc801b1fec0fc09eea0b77e3a30d6a
ncw <ncw2k69@gmail.com>
2020-07-07 18:36:25 +0300

    init

---
+++ src/scripts.spec.js
@@ -0,0 +1,11 @@
+const adder = (a, b) => {
+    return a + b;
+}
+
+describe('Test', () => {
+    it('addder - 2 + 3 = 5', () => {
+        // direct test
+
+        // false positive check
+    })
+})
